68b1800e4a31e4f3fdd2cf981cc5792665c0810a
TreeGee73 <TreeGee73@gmail.com>
2020-11-12 14:30:17 -0700

    added script.js, moved index.html to main directory

---
+++ Assets/script.js
@@ -0,0 +1,21 @@
+// Variables
+
+const hour1 = $('#time1').text();
+const hour2 = $('#time2').text();
+const hour3 = $('#time3').text();
+const hour4 = $('#time4').text();
+const hour5 = $('#time5').text();
+const hour6 = $('#time6').text();
+const hour7 = $('#time7').text();
+const hour8 = $('#time8').text();
+const hour9 = $('#time9').text();
+const hour10 = $('#time10').text();
+const hour11 = $('#time11').text();
+const hour12 = $('#time12').text();
+
+
+// Create an hourly planner that will allow the user to store events at each hour
+// Create hourly list (6am - 6pm)
+// Create event recording text area for each hour
+// Create a save button for each event to write it to local storage
+// Utilize an API to high light each event during the hour it occurs
